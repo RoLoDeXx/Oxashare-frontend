6810ea3de7bbc3fff4ad73a211ed3e04ebed08f0
RoLoDeXx <rolodexx@gitlab.com>
2020-04-16 00:05:16 +0530

    added validation and showing errors

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 import { TextField, Button } from "@material-ui/core";
+
 class StreamCreate extends Component {
-  renderInput = ({ input, label }) => {
+  renderInput = ({ input, label, meta }) => {
     return (
       <div className="my-3">
-        <TextField {...input} label={label} variant="outlined" />
+        <TextField
+          className="w-100"
+          {...input}
+          label={label}
+          variant="outlined"
+          autoComplete="off"
+        />
+        {meta.error && meta.touched && (
+          <div className=" my-3 alert alert-danger" role="alert">
+            {meta.error}
+          </div>
+        )}
       </div>
     );
   };
@@ -32,12 +44,19 @@ class StreamCreate extends Component {
 }
 
 const validate = (formValues) => {
+  const errors = {};
   if (!formValues.title) {
+    errors.title = "You must add a title!";
   }
 
-  return {};
+  if (!formValues.disc) {
+    errors.disc = "You must add a description!";
+  }
+
+  return errors;
 };
 
 export default reduxForm({
   form: "stream create",
+  validate,
 })(StreamCreate);
