fb7c3b0d9a9a6138b20fd4326f07294d147186f2
RoLoDeXx <rolodexx@gitlab.com>
2020-04-26 02:09:46 +0530

    all actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,30 @@ export const createStream = (formValues) => async (dispatch) => {
   dispatch({ type: "CREATE_STREAM", payload: res.data });
 };
 
+export const fetchStreams = () => async (dispatch) => {
+  const res = await streams.get("/streams");
+
+  dispatch({ type: "FETCH_STREAMS", payload: res.data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const res = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: "FETCH_STREAM", payload: res.data });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const res = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: "EDIT_STREAM", payload: res.data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: "DELETE_STREAM", payload: id });
+};
+
 export const signIn = (userId) => {
   return {
     type: "SIGN_IN",
