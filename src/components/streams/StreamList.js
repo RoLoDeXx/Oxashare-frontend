e63cccb4c99f24b4df65bb36bcc2f0f378443245
RoLoDeXx <rolodexx@gitlab.com>
2020-05-02 10:51:26 +0530

    port to pc

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,33 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
-export default function StreamList() {
-  return <div>list</div>;
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  renderList = () => {
+    return this.props.streams.map((stream) => (
+      <div className="item my-2" key={stream.id}>
+        {stream.title}
+        <br></br>
+        {stream.disc}
+      </div>
+    ));
+  };
+
+  render() {
+    console.log(this.props.streams);
+
+    return <div>{this.renderList()}</div>;
+  }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    streams: Object.values(state.streams),
+  };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
